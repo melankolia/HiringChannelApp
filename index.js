a627a8d9d5c65e1fa9a35aaaf470725d039329ff
melankolia <setyo.nugroho96@gmail.com>
2020-01-05 21:31:30 +0700

    first

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv/config");
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const bodyParser = require("body-parser");
 
 const helmet = require("helmet");
