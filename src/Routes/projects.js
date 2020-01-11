087e64edf77c22cadbc54ebf664afbaa8ed77162
melankolia <setyo.nugroho96@gmail.com>
2020-01-12 04:55:41 +0700

    Fixing Title

--- src/Routes/projects.js
+++ src/Routes/projects.js
@@ -3,9 +3,9 @@ const controller = require("../Controllers/projects");
 
 const Router = express.Router();
 
-Router.get("/", controller.getprojects); //localhost:8000/api/engineer/projects
-Router.get("/:id", controller.getprojectslength) ; //localhost:8000/api/engineer/projects/:id
-Router.patch("/:No", controller.patchprojects); //localhost:8000/api/engineer/projects
+Router.get("/", controller.getprojects); //localhost:8000/api/projects
+Router.get("/:id", controller.getprojectslength) ; //localhost:8000/api/projects/:id
+Router.patch("/:No", controller.patchprojects); //localhost:8000/api/projects
 Router.post("/", controller.postproject)
 
 module.exports = Router;
